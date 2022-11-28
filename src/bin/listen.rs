a14762e6a816a492c0f93e64a8c41832c2836cd7
Ruediger Klaehn <rklaehn@protonmail.com>
2022-11-28 14:04:10 +0200

    Make buffer bigger, and add throughput bench

--- src/bin/listen.rs
+++ src/bin/listen.rs
@@ -1,16 +1,19 @@
-use std::os::unix::net::UnixDatagram;
+use std::{os::unix::net::UnixDatagram, time::Instant};
 
 fn main() -> anyhow::Result<()> {
     std::fs::remove_file("a.socket")?;
     let socket = UnixDatagram::bind("a.socket")?;
-    let mut buf = [0; 1024 * 2];
+    let mut buf = [0; 1024 * 16];
     let mut i: u64 = 0;
+    let t0 = Instant::now();
     loop {
         let (size, src) = socket.recv_from(&mut buf)?;
         assert!(size == buf.len());
         i += 1;
         if i % 1000 == 0 {
-            println!("{} {}", i, i * 1024);
+            let elapsed = t0.elapsed();
+            let data = i * 1024;
+            println!("{} {}", i, data as f64 / elapsed.as_secs_f64());
         }
     }
     Ok(())
--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -3,7 +3,7 @@ use std::{os::unix::net::UnixDatagram, time::Duration};
 fn main() -> anyhow::Result<()> {
     std::fs::remove_file("b.socket")?;
     let socket = UnixDatagram::bind("b.socket")?;
-    let buf = [0; 1024 * 2];
+    let buf = [0; 1024 * 16];
     let mut i: u64 = 0;
     loop {
         match socket.send_to(&buf, "a.socket") {
